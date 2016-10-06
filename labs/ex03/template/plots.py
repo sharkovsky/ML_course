6fbf2f648af8ff6d751b21da980bb3325397ce82
brunomaga <brunomaga@gmail.com>
2016-10-06 15:48:28 +0200

    added solutions for ex03

--- labs/ex03/template/plots.py
+++ labs/ex03/template/plots.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 """a function of ploting figures."""
 import numpy as np
-from build_polynomial import build_poly
+#from build_polynomial import build_poly
 
+#NOTE we copied this from our python notebook
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    basis_matrix = np.zeros(shape=(len(x), degree+1))
+    for i in range(0,len(x)):
+        for j in range(0,degree+1):
+            basis_matrix[i,j] = x[i]**j 
+    return basis_matrix
 
 def plot_fitted_curve(y, x, weights, degree, ax):
     """plot the fitted curve."""
