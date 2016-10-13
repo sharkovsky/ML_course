83c5e902193991047086ef4ab128bf4f22839da0
cremones <francesco.cremonesi@epfl.ch>
2016-10-13 14:34:02 +0200

    making merge happy

--- labs/ex03/template/plots.py
+++ labs/ex03/template/plots.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """a function of ploting figures."""
 import numpy as np
+<<<<<<< HEAD
 #from build_polynomial import build_poly
+=======
+from build_polynomial import *
+import matplotlib.pyplot as plt
+>>>>>>> 7c0c1537357986b495afe9643dc0b5fb76a75224
 
 #NOTE we copied this from our python notebook
 def build_poly(x, degree):
@@ -22,3 +27,21 @@ def plot_fitted_curve(y, x, weights, degree, ax):
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_title("Polynomial degree " + str(degree))
+
+def plot_train_test(train_errors, test_errors, lambdas, degree):
+    """
+    train_errors, test_errors and lambas should be list (of the same size) the respective train error and test error for a given lambda,
+    * lambda[0] = 1
+    * train_errors[0] = RMSE of a ridge regression on the train set
+    * test_errors[0] = RMSE of the parameter found by ridge regression applied on the test set
+    
+    degree is just used for the title of the plot.
+    """
+    plt.semilogx(lambdas, train_errors, color='b', marker='*', label="Train error")
+    plt.semilogx(lambdas, test_errors, color='r', marker='*', label="Test error")
+    plt.xlabel("lambda")
+    plt.ylabel("RMSE")
+    plt.title("Ridge regression for polynomial degree " + str(degree))
+    leg = plt.legend(loc=1, shadow=True)
+    leg.draw_frame(False)
+    plt.savefig("ridge_regression")
